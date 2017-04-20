02640c250d4d5ebb06f8c7f9cccb8ed2ef90f670
1308006 <lf20204@gmail.com>
2017-04-20 15:38:19 +0800

    remove unused import

--- webapp/web-final/src/main/java/com/iisigroup/web/UserController.java
+++ webapp/web-final/src/main/java/com/iisigroup/web/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
